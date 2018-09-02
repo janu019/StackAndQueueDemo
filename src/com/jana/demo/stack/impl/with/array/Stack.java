7f3f3cc4a579fbbf7e8fcdc7937b5201a4830bb9
janareddy <janareddy@overstock.com>
2018-09-01 23:01:53 -0600

    Stack testing using main method added

--- src/com/jana/demo/stack/impl/with/array/Stack.java
+++ src/com/jana/demo/stack/impl/with/array/Stack.java
@@ -4,43 +4,44 @@ public class Stack {
 	private final int MAX_SIZE = 3;
 	public char[] array = new char[MAX_SIZE];
 	public int size = 0;
-	
+
 	public boolean isEmpty() {
 		return size == 0;
 	}
-	
+
 	public boolean isFull() {
 		return size == MAX_SIZE;
 	}
-	
+
 	public boolean push(char c) {
-		if(isFull()) {
+		if (isFull()) {
 			System.out.println("StackFullException");
 			return false;
 		}
-		
-		array[size ++] = c;
+
+		System.out.println("Pushing " + c + " to the stack");
+		array[size++] = c;
 		return true;
 	}
-	
+
 	public char pop() {
-		if(isEmpty()) {
-			System.out.println("StackEmptyException");
-		}
-		
-		char c = array[size -1];
-		size --;
-		//array[size -1] =  null;
-		return c;
-	}
-	
-	public char top() {
-		if(isEmpty()) {
+		if (isEmpty()) {
 			throw new RuntimeException("StackEmptyException");
 		}
-		
-		return array[size -1];
+
+		char c = array[size - 1];
+		size--;
+		// Assign it to null - so that the reference get Garbage collected
+		// array[size -1] = null;
+		return c;
 	}
-	
-	
+
+	public char top() {
+		if (isEmpty()) {
+			throw new RuntimeException("StackEmptyException");
+		}
+
+		return array[size - 1];
+	}
+
 }
--- src/com/jana/demo/stack/impl/with/array/StackTest.java
+++ src/com/jana/demo/stack/impl/with/array/StackTest.java
@@ -7,12 +7,19 @@ public class StackTest {
 		
 		System.out.println("Should be empty : " + stack.isEmpty());
 		System.out.println("Should not be full : " + stack.isFull());
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		stack.push('A');
+		stack.push('B');
+		stack.push('C');
+		stack.push('D');
+		System.out.println("Top Should be 'C' : " + stack.top());
+		System.out.println("Should be full : " + stack.isFull());
+		System.out.println("Popping " + stack.pop() + " From Stack");
+		System.out.println("Popping " + stack.pop() + " From Stack");
+		System.out.println("Popping " + stack.pop() + " From Stack");
+		System.out.println("Should be empty : " + stack.isEmpty());
+		System.out.println("Should not be full : " + stack.isFull());
+		//Should throw StackEmptyException
+		System.out.println(stack.pop());
 	}
 
 }
